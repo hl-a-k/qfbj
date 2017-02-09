b9b52d80e2893e1cb4061fb6405968506916cc8c
hl_a_k@163.com <hl_a_k@163.com>
2017-02-09 13:51:08 +0800

    + 增加sqlite支持

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,9 +3,21 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './Home.css';
 
+var sqlite3 = require('sqlite3');
 
 export default class Home extends Component {
   render() {
+
+
+    var  db = new sqlite3.Database('E://zyf.db');
+    db.all('select * from t_sys_user',function(err,res){
+      console.log(res)
+    })
+
+    db.close();
+
+
+
     return (
       <div>
         <div className={styles.container}>
